eef135b11cafe889ab962a0756d4c3f04fb8b5a5
Angela Sun <angelasun1995@gmail.com>
2022-03-30 17:33:08 -0400

    Allow players to click the board and place their symbol

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 const board = (() => {
-  const gameBoard = [];
+  const gameBoard = [null, null, null, null, null, null, null, null, null];
   const placeAt = (player, index) => {
     gameBoard[index] = player.sign;
     console.log(gameBoard);
   };
-  return { placeAt };
+  const isEmpty = (index) => {
+    return gameBoard[index] === null;
+  };
+  return { placeAt, isEmpty };
 })();
 
 const player = (sign) => {
   return { sign };
 };
 
-const playerA = player("O");
-const playerB = player("X");
-
 const game = (() => {
+  const playerA = player("O");
+  const playerB = player("X");
+
+  let nextTurn = false;
+
+  const getCurrentPlayer = () => {
+    return nextTurn ? playerB : playerA;
+  };
+  const nextPlayer = (nextTurn) => {
+    return nextTurn ? playerA : playerB;
+  };
+
   const start = () => {
     displayController.init();
   };
-  return { start };
+
+  const validateMove = (index) => {
+    if (board.isEmpty(index)) {
+      updateMove(index);
+    }
+  };
+
+  const updateMove = (index) => {
+    board.placeAt(getCurrentPlayer(), index);
+    displayController.updateBoard(getCurrentPlayer(), index);
+    nextTurn = !nextTurn;
+  };
+
+  return { start, validateMove };
 })();
 
 const displayController = (() => {
@@ -26,19 +51,24 @@ const displayController = (() => {
     const board = displayBoard();
   };
 
+  const updateBoard = (player, index) => {
+    const square = document.querySelector(`[data-index="${index}"]`);
+    square.textContent = player.sign;
+  };
+
   const displayBoard = () => {
     const grid = document.querySelector(".grid");
     for (let i = 0; i < 9; i++) {
       const square = document.createElement("div");
-      square.addEventListener("click", () => {
-        console.log("clicked!");
+      square.addEventListener("click", (event) => {
+        game.validateMove(event.target.getAttribute("data-index"));
       });
       square.classList.add("square");
       square.setAttribute("data-index", i);
       grid.appendChild(square);
     }
   };
-  return { init };
+  return { init, updateBoard };
 })();
 
 game.start();
