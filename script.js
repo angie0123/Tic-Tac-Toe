622678bd45454a079cb67b4bc25016a1724d4280
Angela Sun <angelasun1995@gmail.com>
2022-03-30 16:40:43 -0400

    Initiate empty board in js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("connected");
+const board = (() => {
+  const gameBoard = [null, null, null, null, null, null, null, null, null];
+  const placeAt = (player, index) => {
+    gameBoard[index] = player.sign;
+    console.log(gameBoard);
+  };
+  return { placeAt };
+})();
+
+const player = (sign) => {
+  return { sign };
+};
+
+const playerA = player("O");
+const playerB = player("X");
+
+const game = (() => {
+  const start = () => {
+    displayController.init();
+  };
+  return { start };
+})();
+
+const displayController = (() => {
+  const init = () => {
+    const board = displayBoard();
+  };
+
+  const displayBoard = () => {
+    const grid = document.querySelector(".grid");
+    for (let i = 0; i < 9; i++) {
+      const square = document.createElement("div");
+      square.addEventListener("click", () => {
+        console.log("clicked!");
+      });
+      square.classList.add("square");
+      square.setAttribute("data-index", i);
+      grid.appendChild(square);
+    }
+  };
+  return { init };
+})();
+
+game.start();
